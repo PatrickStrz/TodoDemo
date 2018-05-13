dc683159c3bf495204d07dc8e94e62881cd9eadf
Patrick Strzelec <patrick.strzelec@gmail.com>
2018-05-13 02:22:27 -0400

    added first test

--- src/App.test.js
+++ src/App.test.js
@@ -2,12 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
 import { configure, shallow } from 'enzyme'
-import Adapter from 'enzyme-adpater-react-16'
+import Adapter from 'enzyme-adapter-react-16'
 
 configure({ adapter: new Adapter() })
 
 describe('<App/>', () => {
-  it('should render App', () => {
+  it('should render App wrapper', () => {
     const app = shallow(<App />)
     console.log(app.debug())
   })
