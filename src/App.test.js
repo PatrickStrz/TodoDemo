ef6334a0d783324f5077ba8a8beb9a8e96537519
Patrick Strzelec <patrick.strzelec@gmail.com>
2018-05-12 20:52:06 -0400

    added enzyme

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
+import { configure } from 'enzyme'
+import Adapter from 'enzyme-adpater-react-16'
+
+configure({ adapter: new Adapter() })
 
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+  const div = document.createElement('div')
+  ReactDOM.render(<App />, div)
+  ReactDOM.unmountComponentAtNode(div)
+})
